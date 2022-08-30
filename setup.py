42c280613dbdb39d2e4848d7b25c4e99671e6464
Muhammadali-Akbarov <muhammadali17abc@gmail.com>
2022-08-30 23:46:01 +0500

    Modified PyPi Confs⚙️

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='paymentsuz',
-    version='1.1',
+    name='payme-pkg',
+    version='1.2',
     license='MIT',
     author="Muhammadali Akbarov",
     author_email='muhammadali17abc@gmail.com',
     packages=find_packages('lib'),
     package_dir={'': 'lib'},
-    url='https://github.com/Muhammadali-Akbarov/payme_uz',
+    url='https://github.com/Muhammadali-Akbarov/payme_pkg',
     keywords='paymeuz paycomuz',
     install_requires=[
           'requests',
