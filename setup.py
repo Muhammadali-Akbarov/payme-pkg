9c7600d2d3dede54c67c4f296e3239f2229079e5
Muhammadali-Akbarov <muhammadali17abc@gmail.com>
2022-09-02 11:27:38 +0500

    Small Changes 👼🏻

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='payme-pkg',
-    version='1.4',
+    version='1.5',
     license='MIT',
     author="Muhammadali Akbarov",
     author_email='muhammadali17abc@gmail.com',
     packages=find_packages('lib'),
     package_dir={'': 'lib'},
-    url='https://github.com/Muhammadali-Akbarov/payme_pkg',
+    url='https://github.com/Muhammadali-Akbarov/payme-pkg',
     keywords='paymeuz paycomuz',
     install_requires=[
           'requests',
