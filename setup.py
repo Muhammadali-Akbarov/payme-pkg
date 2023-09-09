958f2af6703488ca103f130d3fa28ac3b060947e
Muhammadali <79622590+Muhammadali-Akbarov@users.noreply.github.com>
2023-09-09 12:52:26 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='payme-pkg',
-    version='2.5.3',
+    version='2.5.8',
     license='MIT',
     author="Muhammadali Akbarov",
     author_email='muhammadali17abc@gmail.com',
