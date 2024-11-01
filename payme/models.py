6ce8c4cd89ea4d59973b62d3a8f34f3c8fe53fa0
muhammadalive <muhammadali17abc@gmail.com>
2024-11-01 15:19:45 +0500

    (fixed) cancel reason

--- payme/models.py
+++ payme/models.py
@@ -42,7 +42,7 @@ class PaymeTransactions(models.Model):
     )
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     state = models.IntegerField(choices=STATE, default=CREATED)
-    cancel_reason = models.IntegerField()
+    cancel_reason = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
     performed_at = models.DateTimeField(null=True, blank=True, db_index=True)
