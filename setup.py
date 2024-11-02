bf081fca990d87b36870007d64420f913008ab71
muhammadalive <muhammadali17abc@gmail.com>
2024-11-02 08:45:55 +0500

    small fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='payme-pkg',
-    version='3.0.1',
+    version='3.0.2',
     license='MIT',
     author="Muhammadali Akbarov",
     author_email='muhammadali17abc@gmail.com',
