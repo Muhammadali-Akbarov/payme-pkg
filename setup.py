cf9d5645436e478cc5b3e7b6dbb61539e00dc950
muhammadalive <muhammadali17abc@gmail.com>
2024-11-24 12:32:14 +0500

    small fix and improvements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='payme-pkg',
-    version='3.0.12',
+    version='3.0.13',
     license='MIT',
     author="Muhammadali Akbarov",
     author_email='muhammadali17abc@gmail.com',
